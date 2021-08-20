c802800c0a4135463f74e3d8b8f8965e27841607
ptamborindeguy <ptamborindeguy@medallia.com>
2021-08-20 18:03:04 -0300

    Naming refactor. Added diagonal check. Added tied games. Added testing for this changes

--- backend/models/ConnectFourGame.py
+++ backend/models/ConnectFourGame.py
@@ -11,7 +11,6 @@ class ConnectFourGame:
         self._current_player = 1
         self._is_over = False
         self._was_won = False
-        self._was_tied = False
         self._winner = None
 
     @property
@@ -31,62 +30,65 @@ class ConnectFourGame:
         return self._was_won
 
     @property
-    def was_tied(self):
-        return self._was_tied
-
-    @property
     def winner(self):
         return self._winner if self.is_over else None
+
+    @property
+    def was_tied(self):
+        return not self.was_won and self._board_is_full()
 
     @property
     def is_over(self):
         return self.was_won or self.was_tied
 
-    def put_on(self, column_number):
-        # validate move
+    def drop_checker_on_column(self, column_number):
+        self._validate_move(column_number)
+        self._update_game(column_number)
+
+    def _validate_move(self, column_number):
         if not 0 <= column_number < self._width:
             raise ConnectFourException("Can't play on column bigger or equal than the width, or less than 0.")
 
-        if self._next_free_row_number_by_column[column_number] >= self._height:
+        if self._next_free_row_number_by_column[column_number] >= self.height:
             raise ConnectFourException(f'Column {column_number} is full.')
 
+        if self.is_over:
+            raise ConnectFourException("Can't play, game is over!")
+
+    def _update_game(self, column_number):
         self._update_board_with_move_on(self._current_player, column_number)
         self._update_if_was_won()
         self._update_current_player()
 
     def _update_board_with_move_on(self, player_number, column_number):
         next_free_row_number = self._next_free_row_number_by_column[column_number]
-        # self._board[column_number][next_free_row_number] = self._current_player
         self._board[column_number][next_free_row_number] = player_number
         self._next_free_row_number_by_column[column_number] = next_free_row_number + 1
 
     def _update_if_was_won(self):
-        # check if current player won horizontally
-        for x, y in itertools.product(range(self.width - 3), range(self.height)):
-            if self._board[x][y] != 0 and self._board[x][y] == self._board[x + 1][y] == self._board[x + 2][y] == self._board[x + 3][y]:
-                self._was_won = True
-                self._winner = self._board[x][y]
-                self._is_over = True
-                break
+        horizontal_winner = self._board_winner(range(self.width - 3), range(self.height), 1, 0)
+        vertical_winner = self._board_winner(range(self.width), range(self.height - 3), 0, 1)
+        main_diagonal_winner = self._board_winner(range(self.width - 3), range(self.height - 3), 1, 1)
+        anti_diagonal_winner = self._board_winner(range(3, self._width), range(self.height - 3), -1, 1)
+        potential_winners = [horizontal_winner, vertical_winner, main_diagonal_winner, anti_diagonal_winner]
+        if any(potential_winners):
+            self._was_won = True
+            self._winner = [winner for winner in potential_winners if winner is not None][0]
+            self._is_over = True
 
-        # check if current player won vertically
-        for x, y in itertools.product(range(self.width), range(self.height - 3)):
-            if self._board[x][y] != 0 and self._board[x][y] == self._board[x][y + 1] == self._board[x][y + 2] == self._board[x][y + 3]:
-                self._was_won = True
-                self._winner = self._board[x][y]
-                self._is_over = True
-                break
-
-        # check if current player won bottom-top diagonally
-        for x, y in itertools.product(range(self.width - 3), range(self.height - 3)):
-            if self._board[x][y] != 0 and self._board[x][y] == self._board[x + 1][y + 1] == self._board[x + 2][y + 2] == self._board[x + 3][y + 3]:
-                self._was_won = True
-                self._winner = self._board[x][y]
-                self._is_over = True
-                break
+    def _board_winner(self, x_range, y_range, x_increment, y_increment):
+        for x, y in itertools.product(x_range, y_range):
+            line = [self._board[x + i * x_increment][y + i * y_increment] for i in range(4)]
+            all_elements_in_line_are_equal = len(set(line)) == 1
+            if line[0] != 0 and all_elements_in_line_are_equal:
+                return self._board[x][y]
+        return None
 
     def _update_current_player(self):
         self._current_player = 1 if self.current_player == 2 else 2
+
+    def _board_is_full(self):
+        return all([self._board[x][y] for (x, y) in list(itertools.product(range(self.width), range(self.height)))])
 
     def print_board_state(self):
         return '\n'.join([' '.join([str(self._board[x][y]) for x in range(self.width)]) for y in reversed(range(self.height))])
--- backend/models/tests/test_connect_four_game.py
+++ backend/models/tests/test_connect_four_game.py
@@ -11,14 +11,14 @@ def test_board_is_created_with_correct_dimensions():
 def test_cant_put_in_column_bigger_or_equal_than_width():
     game = ConnectFourGame(7, 6)
     with pytest.raises(ConnectFourException) as ex:
-        game.put_on(7)
+        game.drop_checker_on_column(7)
     assert (str(ex.value)) == "Can't play on column bigger or equal than the width, or less than 0."
 
 
 def test_cant_put_in_column_smaller_than_0():
     game = ConnectFourGame(7, 6)
     with pytest.raises(ConnectFourException) as ex:
-        game.put_on(-1)
+        game.drop_checker_on_column(-1)
     assert (str(ex.value)) == "Can't play on column bigger or equal than the width, or less than 0."
 
 
@@ -29,16 +29,16 @@ def test_starts_playing_1():
 
 def test_current_player_is_2_after_player_1():
     game = ConnectFourGame(7, 6)
-    game.put_on(5)
+    game.drop_checker_on_column(5)
     assert game.current_player == 2
 
 
 def test_plays_1_after_2():
     game = ConnectFourGame(7, 6)
     first_turn_player = game.current_player
-    game.put_on(5)
+    game.drop_checker_on_column(5)
     second_turn_player = game.current_player
-    game.put_on(6)
+    game.drop_checker_on_column(6)
     assert first_turn_player == 1
     assert second_turn_player == 2
     assert game.current_player == 1
@@ -50,7 +50,7 @@ def test_cant_put_more_discs_in_same_column_than_board_height():
     column_number = 0
     with pytest.raises(ConnectFourException) as ex:
         for i in range(height + 1):
-            game.put_on(column_number)
+            game.drop_checker_on_column(column_number)
     assert (str(ex.value)) == f'Column {column_number} is full.'
 
 
@@ -65,7 +65,7 @@ def test_player_1_wins_horizontally_at_the_beginning_of_first_row():
     1 1 1 1 0 0 0
     """
     game = ConnectFourGame(7, 6)
-    [game.put_on(c) for c in [0, 0, 1, 1, 2, 2, 3]]
+    [game.drop_checker_on_column(c) for c in [0, 0, 1, 1, 2, 2, 3]]
     assert game.was_won
     assert game.winner == 1
     assert game.is_over
@@ -89,7 +89,7 @@ def test_player_1_wins_horizontally_at_the_end_of_first_row():
     1 2 2 1 1 1 1
     """
     game = ConnectFourGame(7, 6)
-    [game.put_on(c) for c in [0, 1, 0, 1, 3, 0, 4, 2, 5, 2, 6]]
+    [game.drop_checker_on_column(c) for c in [0, 1, 0, 1, 3, 0, 4, 2, 5, 2, 6]]
     assert game.was_won
     assert game.winner == 1
     assert game.is_over
@@ -113,7 +113,7 @@ def test_player_1_wins_vertically_at_the_beginning_of_first_column():
     1 2 0 0 0 0 0
     """
     game = ConnectFourGame(7, 6)
-    [game.put_on(c) for c in [0, 1, 0, 1, 0, 1, 0]]
+    [game.drop_checker_on_column(c) for c in [0, 1, 0, 1, 0, 1, 0]]
     assert game.was_won
     assert game.winner == 1
     assert game.is_over
@@ -137,7 +137,7 @@ def test_player_1_wins_vertically_at_end_of_first_column():
     2 1 2 0 0 0 0
     """
     game = ConnectFourGame(7, 6)
-    [game.put_on(c) for c in [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 2, 0]]
+    [game.drop_checker_on_column(c) for c in [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 2, 0]]
     assert game.was_won
     assert game.winner == 1
     assert game.print_board_state() == """\
@@ -160,7 +160,7 @@ def test_player_1_wins_diagonally_at_beginning_of_the_main_diagonal():
     1 2 2 1 0 0 0
     """
     game = ConnectFourGame(7, 6)
-    [game.put_on(c) for c in [3, 3, 3, 2, 3, 2, 0, 1, 1, 0, 2]]
+    [game.drop_checker_on_column(c) for c in [3, 3, 3, 2, 3, 2, 0, 1, 1, 0, 2]]
     assert game.was_won
     assert game.winner == 1
     assert game.is_over
@@ -184,7 +184,7 @@ def test_player_1_wins_diagonally_at_end_of_the_main_diagonal():
     0 0 0 1 2 2 1
     """
     game = ConnectFourGame(7, 6)
-    [game.put_on(c) for c in [3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 5, 6, 6, 5, 6, 6, 5, 6]]
+    [game.drop_checker_on_column(c) for c in [3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 5, 6, 6, 5, 6, 6, 5, 6]]
     assert game.was_won
     assert game.winner == 1
     assert game.is_over
@@ -195,3 +195,74 @@ def test_player_1_wins_diagonally_at_end_of_the_main_diagonal():
 0 0 0 1 2 2 2
 0 0 0 2 1 1 1
 0 0 0 1 2 2 1"""
+
+
+def test_player_1_wins_diagonally_at_the_beginning_of_the_anti_diagonal():
+    """
+    Board final state is
+    0 0 0 0 0 0 0
+    0 0 0 0 0 0 0
+    0 0 0 1 0 0 0
+    0 0 0 2 1 0 0
+    0 0 0 1 1 1 2
+    0 0 0 2 2 2 1
+    """
+    game = ConnectFourGame(7, 6)
+    [game.drop_checker_on_column(c) for c in [6, 5, 5, 4, 4, 6, 4, 3, 3, 3, 3]]
+    assert game.was_won
+    assert game.winner == 1
+    assert game.is_over
+    assert game.print_board_state() == """\
+0 0 0 0 0 0 0
+0 0 0 0 0 0 0
+0 0 0 1 0 0 0
+0 0 0 2 1 0 0
+0 0 0 1 1 1 2
+0 0 0 2 2 2 1"""
+
+
+def test_cant_play_after_game_is_over():
+    """
+    Board final state is
+    0 0 0 0 0 0 0
+    0 0 0 0 0 0 0
+    0 0 0 0 0 0 0
+    0 0 0 0 0 0 0
+    2 2 2 0 0 0 0
+    1 1 1 1 0 0 0
+    """
+    game = ConnectFourGame(7, 6)
+    [game.drop_checker_on_column(c) for c in [0, 0, 1, 1, 2, 2, 3]]
+    assert game.was_won
+    assert game.winner == 1
+    assert game.is_over
+    with pytest.raises(ConnectFourException) as ex:
+        game.drop_checker_on_column(4)
+    assert (str(ex.value)) == "Can't play, game is over!"
+
+
+def test_game_was_tied():
+    """
+    Board final state is
+    2 2 2 1 2 2 2
+    1 1 1 2 1 1 1
+    2 2 2 1 2 2 2
+    1 1 1 2 1 1 1
+    2 2 2 1 2 2 2
+    1 1 1 2 1 1 1
+    """
+    game = ConnectFourGame(7, 6)
+    [game.drop_checker_on_column(c) for c in [0, 0, 1, 1, 2, 2, 0, 0, 1, 1, 2, 2, 0, 0, 1, 1, 2, 2, 4, 3, 3, 4, 5]]
+    [game.drop_checker_on_column(c) for c in [3, 3, 3, 3]]
+    [game.drop_checker_on_column(c) for c in [5, 6, 6, 4, 4, 5, 5, 6, 6, 4, 4, 5, 5, 6, 6]]
+    assert game.is_over
+    assert game.was_tied
+    assert not game.was_won
+    assert game.winner is None
+    assert game.print_board_state() == """\
+2 2 2 1 2 2 2
+1 1 1 2 1 1 1
+2 2 2 1 2 2 2
+1 1 1 2 1 1 1
+2 2 2 1 2 2 2
+1 1 1 2 1 1 1"""
